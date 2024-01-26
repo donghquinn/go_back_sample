d5778d89edf13d3a84f0b8e6b0b4d7b1cacc343c
donghquinn <ehdgus1524@gmail.com>
2024-01-26 17:01:10 +0900

    wip: comments

--- controllers/calculate.go
+++ controllers/calculate.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Request Type Definition
 type CalculateRequest struct {
 	Num1 int `form:"num1" json:"num1" xml:"num1" binding:"required"`
 	Num2 int `form:"num2" json:"num2" xml:"num2" binding:"required"`
@@ -19,15 +20,15 @@ func CalculateAddController(ctx *gin.Context) {
 	var request CalculateRequest
 
 	if bodyErr := ctx.ShouldBind(&request); bodyErr != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Required Value is Not Included","error": bodyErr.Error()})
+		ctx.JSON(http.StatusBadRequest, gin.H{"message": "Required Value is Not Included", "error": bodyErr.Error()})
 
 		return
-	}  
+	}
+
 	num1 := request.Num1
 	num2 := request.Num2
 
 	fmt.Printf("[ADD] Body Data Num1: %o, Num2: %o\n", num1, num2)
-
 
 	value := calculator.Add(float64(num1), float64(num2))
 
